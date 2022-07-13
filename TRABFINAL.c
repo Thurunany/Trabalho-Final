202b2fb1e3048d36bdf4bdaf602697b0cbb585c1
Thurunany <thutymsoares@gmail.com>
2022-07-13 01:03:04 -0300

    Organizando melhor o contador

--- TRABFINAL.c
+++ TRABFINAL.c
@@ -2,19 +2,13 @@
 #include <stdio.h>
 #include <time.h>
 
-// criar uma variavel que salve os jogos jogados
-// se jogar mais de 5 move manualmente a memoria
-// manualmente mover 
-
-
-int tentativa = 0;
+int tentativas = 0;
 int i;
 int historico[4];
 
-int jogo(int tentativas){
+int jogo(){
     int numPlayer = 0;
-    int indice;
-
+    int pos = 0;
     //numero aleatorio
     // srand(time(NULL));
     // int numSecreto = rand() % 100 + 1;
@@ -35,34 +29,33 @@ int jogo(int tentativas){
         tentativas++;
         scanf("%d", &numPlayer);       
     }}
+    
+    for(i=4-1;i>=pos-1;i--){
+        historico[i+1]=historico[i];
+        historico[pos-1]= tentativas;
+    }    
+    
+    return tentativas;
+}
 
+int contador(){
     int pos = 0;
     int size = 4;
     
-    // for(i=0;i<=4;i++){
-    //     historico[i-1]=historico[i];
-    //     historico[i]= tentativas;
-    // }
-
-    for(i=size-1;i>=pos-1;i--){
-         historico[i+1]=historico[i];
-         historico[pos-1]= tentativas;
+    for(i=0;i<=4;i++){
+        printf("Jogo %d:  ",i+1);
+        printf("%d tentativas\n", historico[i]);
     }
-
-
-return tentativas;
 }
+
 
 int escolha(int opcao){
     switch(opcao){
          case 1:{
-            return printf("Parabéns! Você acertou em %d tentativas!\n", jogo(tentativa));
+            return printf("Parabéns! Você acertou em %d tentativas!\n", jogo(tentativas));
          } break;
          case 2:{
-            for(i=0;i<=4;i++){
-                printf("Jogos %d:  ",i+1);
-                printf("%d tentativas\n", historico[i]);
-            }
+            return printf("Este é seu historico!\n", contador());
          } break;
          case 3:{
             return printf("Limpar histórico\n");
@@ -79,14 +72,13 @@ int main(){
 
     //Jogador escolhe opção:
     while(opcoes != 4){ 
-        printf("Olá jogador! Escolha uma opção:\n 1- Novo Jogo\n 2- Ver últimos 5 jogos!\n 3- Limpar histórico\n 4- Sair\n");
+        printf("\nMENU\nOlá jogador! Escolha uma opção:\n 1- Novo Jogo\n 2- Ver últimos 5 jogos!\n 3- Limpar histórico\n 4- Sair\n");
         scanf("%d", &opcoes);
         while(opcoes < 1 || opcoes > 4) {
             printf("Número errado, escolha uma opção valida!\nTente de novo! \nEscolha uma opção:\n 1- Novo Jogo\n 2- Ver últimos 5 jogos!\n 3- Limpar histórico\n 4- Sair\n");
             scanf("%d", &opcoes);
         }
-        opcoes = escolha(opcoes);
-    }   
-
+        opcoes = escolha(opcoes); 
+    }
     return EXIT_SUCCESS;
 }
